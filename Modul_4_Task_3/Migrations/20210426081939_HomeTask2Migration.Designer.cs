4a5ae7654e46f045b79d8aebbef92f0dec0b91d0
Sobol Andrey <sobol.andrey.25@gmail.com>
2021-04-26 11:19:57 +0300

    add home task 2 migration

--- Modul_4_Task_3/Migrations/20210426081939_HomeTask2Migration.Designer.cs
+++ Modul_4_Task_3/Migrations/20210426081939_HomeTask2Migration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Modul_4_Task_3;
 
 namespace Modul_4_Task_3.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20210426081939_HomeTask2Migration")]
+    partial class HomeTask2Migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Modul_4_Task_3/Migrations/20210426081939_HomeTask2Migration.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Modul_4_Task_3.Migrations
+{
+    public partial class HomeTask2Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClientId",
+                table: "Project",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    ClientId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.ClientId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_ClientId",
+                table: "Project",
+                column: "ClientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_Client_ClientId",
+                table: "Project",
+                column: "ClientId",
+                principalTable: "Client",
+                principalColumn: "ClientId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_Client_ClientId",
+                table: "Project");
+
+            migrationBuilder.DropTable(
+                name: "Client");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Project_ClientId",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "Project");
+        }
+    }
+}
