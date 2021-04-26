faf71f5b8af3a38a07f47a2b18080fb050cecdcc
meridian34 <sobol.andrey.25@gmail.com>
2021-04-26 22:51:44 +0300

    add instance data

--- Modul_4_Task_3/Migrations/20210426081939_HomeTask2Migration.cs
+++ Modul_4_Task_3/Migrations/20210426081939_HomeTask2Migration.cs
@@ -54,6 +54,36 @@ namespace Modul_4_Task_3.Migrations
                     { "5_Project", 9000000, new DateTime(2023, 8, 3, 0, 0, 0, 0), 5 }
                 });
 
+            migrationBuilder.InsertData(
+                table: "Title",
+                columns: new[] { "Name" },
+                values: new object[,]
+                {
+                    { "SysAdmin" },
+                    { "DevOps" },
+                    { "Manager" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Office",
+                columns: new[] { "Title", "Location" },
+                values: new object[,]
+                {
+                    { "City24", "Kharkiv" },
+                    { "Boombastic", "Kharkiv" },
+                    { "Paladium", "Kharkiv" }
+                });
+
+            migrationBuilder.Sql(@$"INSERT INTO Employee(FirstName, LastName, HiredDate, DateOfBirth, OfficeId, TitleId) 
+                                    VALUES('Mark','Zobel', '{new DateTime(2020, 8, 3, 0, 0, 0, 0).ToString("s")}', null, 
+                                    (SELECT TitleId FROM Title WHERE Name = 'SysAdmin'),
+                                    (SELECT OfficeId FROM Office WHERE Title = 'Paladium'))");
+
+            migrationBuilder.Sql(@$"INSERT INTO EmployeeProject(Rate, StartedDate, EmployeeId, ProjectId) 
+                                    VALUES({100000}, '{new DateTime(2021, 8, 15, 0, 0, 0, 0).ToString("s")}', 
+                                    (SELECT EmployeeId FROM Employee WHERE FirstName = 'Mark' and LastName = 'Zobel'),
+                                    (SELECT ProjectId FROM Project WHERE Name = 'Roga&Kopyta_Project'))");
+
             migrationBuilder.CreateIndex(
                 name: "IX_Project_ClientId",
                 table: "Project",
@@ -92,6 +122,35 @@ namespace Modul_4_Task_3.Migrations
                     { "4_Project", 10000, new DateTime(2021, 9, 3, 0, 0, 0, 0) },
                     { "5_Project", 9000000, new DateTime(2023, 8, 3, 0, 0, 0, 0) }
                 });
+
+            migrationBuilder.DeleteData(
+                table: "Title",
+                keyColumns: new[] { "Name" },
+                keyValues: new object[,]
+                {
+                    { "SysAdmin" },
+                    { "DevOps" },
+                    { "Manager" }
+                });
+
+            migrationBuilder.DeleteData(
+                table: "Office",
+                keyColumns: new[] { "Title", "Location" },
+                keyValues: new object[,]
+                {
+                    { "City24", "Kharkiv" },
+                    { "Boombastic", "Kharkiv" },
+                    { "Paladium", "Kharkiv" }
+                });
+
+            migrationBuilder.Sql(@$"DELETE FROM Employee WHERE FirstName='Mark' and
+                                    LastName='Zobel' and HiredDate = '{new DateTime(2020, 8, 3, 0, 0, 0, 0)}' and
+                                    DateOfBirth = null and TitleId = (SELECT TitleId FROM Title WHERE Name = 'SysAdmin') and
+                                    OfficeId = (SELECT OfficeId FROM Office WHERE Title = 'Paladium'))");
+
+            migrationBuilder.Sql(@$"DELETE FROM EmployeeProject WHERE Rate = {100000} and StartedDate = '{new DateTime(2021, 8, 15, 0, 0, 0, 0)}' and 
+                                    EmployeeId = (SELECT EmployeeId FROM Employee WHERE FirstName = 'Mark' and LastName = 'Zobel') and
+                                    ProjectId = (SELECT ProjectId FROM Project WHERE Name = 'Roga&Kopyta_Project'))");
 
             migrationBuilder.DropColumn(
                 name: "ClientId",
